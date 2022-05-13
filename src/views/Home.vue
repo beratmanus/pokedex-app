b71ff909e9db95bbce4031341580b4e462dbcc6c
beratkaanmanus <beratkaan@hotmail.be>
2022-05-13 17:47:00 +0200

    setup vue and tailwind

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,33 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div v-for="pokemon in pokemons" :key="pokemon.id">
+      <h2>{{pokemon.name}}</h2>
+    </div>
+    
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+// list op pokemon api : https://stoplight.io/mocks/appwise-be/pokemon/57519009/pokemon
+//details api: https://pokeapi.co/api/v2/pokemon/133  
+
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
-  }
+    
+  },
+  data() {
+    return {
+      pokemons: []
+    }
+  },
+  mounted() {
+    fetch('https://stoplight.io/mocks/appwise-be/pokemon/57519009/pokemon')
+    .then(res => res.json())
+    .then(data => this.pokemons = data)
+    .then(console.log(this.pokemons))
+  },
 }
 </script>
