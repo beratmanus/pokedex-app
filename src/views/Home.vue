8654b84c953f2bc04cdcb70492fc318bb87df930
beratkaanmanus <beratkaan@hotmail.be>
2022-05-14 02:57:13 +0200

    Added search on pokemon name

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="home">
-    <div v-for="pokemon in pokemons" :key="pokemon.id" class="flex p-3 border-2 m-3 rounded-xl justify-around">
+    <input type="text" placeholder="Pokemon zoeken" class="border-2 p-2 text-center rounded-xl m-3" v-model="filterPokemon">
+    <div v-for="pokemon in filteredItems" :key="pokemon.id" class="flex p-3 border-2 m-3 rounded-xl justify-around">
      <div class="flex-col">
         <h2 class="">{{pokemon.name}}</h2>
         <p class="">Nr. {{pokemon.id}}</p>
       </div> 
-      <p class="m-3 text-right">{{pokemon.types[0].type.name}}</p>
-      <p class="m-3 text-right" v-if="pokemon.types.length > 1">{{pokemon.types[1].type.name}}</p>
-      
+          <p class="m-3 text-right">{{pokemon.types[0].type.name}}</p>
+          <p class="m-3 text-right" v-if="pokemon.types.length > 1">{{pokemon.types[1].type.name}}</p>
     </div>
     
   </div>
@@ -27,13 +27,22 @@ export default {
   data() {
     return {
       pokemons: [],
-      
+      filterPokemon: ''
     }
   },
   mounted() {
     fetch(listPokemonUrl)
     .then(res => res.json())
     .then(data => this.pokemons = data)
+  },
+   computed: {
+    filteredItems() {
+      return this.pokemons.filter((pokemon) => {
+        return pokemon.name.toLowerCase().includes(this.filterPokemon.toLowerCase());
+        
+
+      });
+    },
   },
 }
 </script>
